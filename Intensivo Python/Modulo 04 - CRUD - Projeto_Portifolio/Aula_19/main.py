54cc216e37429308fb7547c7cce41dae2e440fdc
Lucas-Sobreira <lucas-andrade2011@live.com>
2024-07-10 19:04:41 -0300

    criando_API_BancoDados

---
+++ Intensivo Python/Modulo 04 - CRUD - Projeto_Portifolio/Aula_19/main.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/jornada")
+def read_jornada():
+    return {"Nossa primeira": "API"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/dobro/{valor}")
+def read_item(valor: int):
+    return {"valor": valor, "dobro": valor * 2}
